40b0ec73a01ee671c21c44a44e18e9bd023e300d
Ojwang-crypto <omondicollins163@gmail.com>
2022-08-03 16:39:40 +0300

    updated header

--- includes/header.php
+++ includes/header.php
@@ -57,8 +57,8 @@
           <li><a href="about.php">About</a></li>
           <li><a href="services.php">Services</a></li>
           <li><a href="pricing.php">Pricing</a></li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
+          <!-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a> -->
+            <!-- <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
                 <ul>
@@ -72,7 +72,7 @@
               <li><a href="#">Drop Down 2</a></li>
               <li><a href="#">Drop Down 3</a></li>
               <li><a href="#">Drop Down 4</a></li>
-            </ul>
+            </ul> -->
           </li>
           <li><a href="contact.php"> Contact</a></li>
           <li><a href="signup.php">SignUp</a></li>
